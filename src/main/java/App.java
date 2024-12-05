4466c8b01c37bd0f46733caf1435eb73b14cdc52
VasmanT <vasman@mail.ru>
2024-12-05 10:33:46 +0300

    Add files via upload

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,21 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+//        System.out.println(bean.getMessage());
+        HelloWorld beanNew =
+                (HelloWorld) applicationContext.getBean("helloworld");
+//        System.out.println(beanNew.getMessage());
+//        System.out.println(bean.toString());
+//        System.out.println(beanNew.toString());
+        System.out.println(bean.equals(beanNew));
+
+//        System.out.println("-------");
+        Cat cat = (Cat) applicationContext.getBean("cat");
+//        System.out.println(cat.getMessage());
+        Cat catNew = (Cat) applicationContext.getBean("cat");
+//        System.out.println(catNew.getMessage());
+//        System.out.println(cat.toString());
+//        System.out.println(catNew.toString());
+        System.out.println(cat.equals(catNew));
     }
 }
